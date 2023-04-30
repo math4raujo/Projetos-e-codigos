5807b6ab3141922446c2a90ae186668ab3ae0d0c
Matheus Lima de Araujo <88353602+math4raujo@users.noreply.github.com>
2023-04-29 22:05:21 -0300

    jogo_da_velha

--- jogodavelha.py
+++ jogodavelha.py
@@ -12,66 +12,71 @@ a_jogada = b_jogada = c_jogada = d_jogada = e_jogada = f_jogada = g_jogada = h_j
 while not jogo_acabou:
 
     # Para o jogador X
-    if jogada_X == 1 and a == 0:
+    if jogada_X == 9 and a == 0:
         a = 3**8
         a_jogada = 1
-    elif jogada_X == 2 and b == 0: 
+    elif jogada_X == 8 and b == 0: 
         b = 3**7
         b_jogada = 1
-    elif jogada_X == 3 and c == 0:
+    elif jogada_X == 7 and c == 0:
         c = 3**6
         c_jogada = 1
-    elif jogada_X == 4 and d == 0:
+    elif jogada_X == 6 and d == 0:
         d = 3**5
         d_jogada = 1
     elif jogada_X == 5 and e == 0:
         e = 3**4
         e_jogada = 1
-    elif jogada_X == 6 and f == 0:
+    elif jogada_X == 4 and f == 0:
         f = 3**3
         f_jogada = 1
-    elif jogada_X == 7 and g == 0:
+    elif jogada_X == 3 and g == 0:
         g = 3**2
         g_jogada = 1
-    elif jogada_X == 8 and h == 0:
+    elif jogada_X == 2 and h == 0:
         h = 3**1
         h_jogada = 1
-    elif jogada_X == 9 and i == 0:
+    elif jogada_X == 1 and i == 0:
         i = 3**0
         i_jogada = 1
 
+    tabuleiro = a + b + c + d + e + f + g + h + i
+    print(tabuleiro)
+    
     # Para o jogador O
     jogada_O = int(input('Jogada do JO: '))
 
-    if jogada_O == 1 and a == 0:
+    if jogada_O == 9 and a == 0:
         a = 2 * 3**8
         a_jogada = 2
-    elif jogada_O == 2 and b == 0: 
+    elif jogada_O == 8 and b == 0: 
         b = 2 * 3**7
         b_jogada = 2
-    elif jogada_O == 3 and c == 0:
+    elif jogada_O == 7 and c == 0:
         c = 2 * 3**6
         c_jogada = 2
-    elif jogada_O == 4 and d == 0:
+    elif jogada_O == 6 and d == 0:
         d = 2 * 3**5
         d_jogada = 2
     elif jogada_O == 5 and e == 0:
         e = 2 * 3**4
         e_jogada = 2
-    elif jogada_O == 6 and f == 0:
+    elif jogada_O == 4 and f == 0:
         f = 2 * 3**3
         f_jogada = 2
-    elif jogada_O == 7 and g == 0:
+    elif jogada_O == 3 and g == 0:
         g = 2 * 3**2
         g_jogada = 2
-    elif jogada_O == 8 and h == 0:
+    elif jogada_O == 2 and h == 0:
         h = 2 * 3**1
         h_jogada = 2
-    elif jogada_O == 9 and i == 0:
+    elif jogada_O == 1 and i == 0:
         i = 2 * 3**0
         i_jogada = 2
 
     tabuleiro = a + b + c + d + e + f + g + h + i
+
+    print(tabuleiro)
 
     # Precisamos checar se alguém venceu
 
